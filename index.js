3c876f6abefbb387c0dd0c5d3d391e50086f585e
SumarPohz <sumarpohz@gmail.com>
2023-07-25 10:24:18 +0530

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function playAudio() {
+function toggleAudio() {
   var audio = document.getElementById("birthdayAudio");
-  audio.play();
+  var playButton = document.getElementById("playButton");
+
+  if (audio.paused) {
+    audio.play();
+    playButton.textContent = "Stop Birthday Music";
+  } else {
+    audio.pause();
+    audio.currentTime = 0; // Rewind the audio to the beginning
+    playButton.textContent = "Play Birthday Music";
+  }
 }
